f01c42b842b6bf6a640864660d3bfad5fe579dc2
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-04-30 16:25:17 +0200

    FEAT: add functionalities for set players, attributes and equipments

--- src/Game.java
+++ src/Game.java
@@ -3,27 +3,65 @@ package src;
 import java.util.ArrayList;
 
 public class Game {
-    Menu menu = new Menu();
+    Menu menu;
+    Player player;
+    Attribute attribute;
+    OffEquipment offEquipment;
+    DefEquipment defEquipment;
 
-    ArrayList<Player> players = new ArrayList<>();
+    ArrayList<Player> players = new ArrayList<Player>();
+    ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+    ArrayList<OffEquipment> offEquipments = new ArrayList<OffEquipment>();
+    ArrayList<DefEquipment> defEquipments = new ArrayList<DefEquipment>();
 
     private int playersCount = 1;
-    private int i = 0;
 
     public Game() {
     }
 
-    public void launch () {
+    public void launch() {
+        menu = new Menu();
         playersCount = menu.start();
 
-        while (i < playersCount) {
-            players.add(menu.create());
-            i++;
+        for (int i = 0; i < playersCount; i++) {
+            player = new Player();
+            players.add(menu.create(player));
         }
 
-        System.out.println("IS CONFIG");
+        for (int i = 0; i < playersCount; i++) {
+            attribute = new Attribute();
 
-        System.out.println(players.get(0).getName() + "\n" + players.get(0).getJob());
-        System.out.println(players.get(1).getName() + "\n" + players.get(1).getJob());
+            if (players.get(i).getJob().equals("WARRIOR")) {
+                attributes.add(new Attribute(attribute.getWarriorBaseStats()));
+            }
+
+            if (players.get(i).getJob().equals("WIZARD")) {
+                attributes.add(new Attribute(attribute.getWizardBaseStats()));
+            }
+        }
+
+        for (int i = 0; i < playersCount; i++) {
+            offEquipment = new OffEquipment();
+            defEquipment = new DefEquipment();
+
+            if (players.get(i).getJob().equals("WARRIOR")) {
+                offEquipments.add(new OffEquipment(offEquipment.getWarriorBaseOffEquipments()));
+                defEquipments.add(new DefEquipment(defEquipment.getWarriorBaseDefEquipments()));
+            }
+
+            if (players.get(i).getJob().equals("WIZARD")) {
+                offEquipments.add(new OffEquipment(offEquipment.getWizardBaseOffEquipments()));
+                defEquipments.add(new DefEquipment(defEquipment.getWizardBaseDefEquipments()));
+            }
+        }
+
+        for (int i = 0; i < players.size(); i++) {
+            System.out.println(players.get(i).getName());
+            System.out.println(players.get(i).getJob());
+            System.out.println("health: " + attributes.get(i).getHealth());
+            System.out.println("strength: " + attributes.get(i).getStrength());
+            System.out.println(offEquipments.get(i).getType() + " : " + offEquipments.get(i).getName() + ", attk: " + offEquipments.get(i).getAttack());
+            System.out.println(defEquipments.get(i).getType() + " : " + defEquipments.get(i).getName() + ", def: " + defEquipments.get(i).getDefense());
+        }
     }
 }
