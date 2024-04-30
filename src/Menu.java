8a25429780451efaaabe3361ab449312c713b5fc
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-04-30 10:19:12 +0200

    FEAT: add all functionalities for start game (added players count, create players...)

--- src/Menu.java
+++ src/Menu.java
@@ -3,41 +3,42 @@ package src;
 import java.util.Scanner;
 
 public class Menu {
-    Player player = new Player();
     Scanner scanner = new Scanner(System.in);
 
     private int menuIndex = 0;
     private String userValue;
-    private boolean isModify = false;
 
     public Menu() {
     }
 
-    public boolean create() {
+    public int start() {
+        menuIndex = newGame();
+        return setPlayersNb();
+    }
+
+    public Player create() {
+        Player player = new Player();
+
         while (true) {
             switch (menuIndex) {
-                case 0:
-                    menuIndex = newGame();
-                    break;
                 case 1:
-                    menuIndex = playerName();
+                    menuIndex = setPlayerName(player, false);
                     break;
                 case 2:
-                    menuIndex = job();
+                    menuIndex = setJob(player, false);
                     break;
                 case 3:
                     menuIndex = jobInfo();
                     break;
                 case 4:
-                    menuIndex = validation();
+                    menuIndex = modify(player);
                     break;
                 case 5:
-                    menuIndex = modify();
+                    menuIndex = validation(player);
                     break;
                 case 6:
-                    return true;
-                default:
-                    System.exit(0);
+                    menuIndex = 1;
+                    return player;
             }
         }
     }
@@ -55,7 +56,7 @@ public class Menu {
                 }
 
                 if (userValue.equals("E")) {
-                    return (-1);
+                    System.exit(0);
                 }
 
                 throw new Exception();
@@ -65,9 +66,22 @@ public class Menu {
         }
     }
 
-    public int playerName() {
+    public int setPlayersNb() {
+        System.out.println("\n\n" + "CHOOSE PLAYERS NUMBER" + "\n");
+        System.out.println("[ENTER] 1 by default");
+
+        userValue = scanner.nextLine().toUpperCase();
+
+        if (userValue.equals("")) {
+            return 1;
+        } else {
+            return Integer.valueOf(userValue);
+        }
+    }
+
+    public int setPlayerName(Player player, boolean isModify) {
         System.out.println("\n\n" + "PLAYER NAME" + "\n");
-        System.out.println("Please enter your player name :");
+        System.out.println("Please enter your Player Name :");
 
         userValue = scanner.nextLine().toUpperCase();
 
@@ -80,7 +94,7 @@ public class Menu {
         }
     }
 
-    public int job() {
+    public int setJob(Player player, boolean isModify) {
         System.out.println("\n\n" + "JOB SELECTION" + "\n");
         System.out.println("Please choose a job between [WARRIOR] or [WIZARD] :" + "\n" + "Job Info [J]");
 
@@ -94,7 +108,12 @@ public class Menu {
 
                 if (userValue.equals("WARRIOR") || userValue.equals("WIZARD")) {
                     player.setJob(userValue);
-                    return 4;
+
+                    if (!isModify) {
+                        return 5;
+                    } else {
+                        return 4;
+                    }
                 }
 
                 throw new Exception();
@@ -126,22 +145,26 @@ public class Menu {
         }
     }
 
-    public int validation() {
-        System.out.println("\n\n" + "CONFIRMATION" + "\n");
-        System.out.println("PLAYER NAME :" + "\n" + player.getName());
-        System.out.println("JOB :" + "\n" + player.getJob());
-        System.out.println("Modify [M]" + "\n" + "Ok [O]");
+    public int modify(Player player) {
+        System.out.println("\n\n" + "MODIFY INFO" + "\n");
+        System.out.println("Player Name [P]" + "\n" + "Job [J]" + "\n" + "Back [B]");
 
         while (true) {
             try {
                 userValue = scanner.nextLine().toUpperCase();
 
-                if (userValue.equals("M")) {
-                    return 5;
+                if (userValue.equals("P")) {
+                    setPlayerName(player, true);
+                    return 4;
                 }
 
-                if (userValue.equals("O")) {
-                    return 6;
+                if (userValue.equals("J")) {
+                    setJob(player, true);
+                    return 4;
+                }
+
+                if (userValue.equals("B")) {
+                    return 5;
                 }
 
                 throw new Exception();
@@ -151,25 +174,22 @@ public class Menu {
         }
     }
 
-    public int modify() {
-        System.out.println("\n\n" + "MODIFY INFO" + "\n");
-        System.out.println("playerName [P]" + "\n" + "Job [J]" + "\n" + "Back [B]");
+    public int validation(Player player) {
+        System.out.println("\n\n" + "CONFIRMATION" + "\n");
+        System.out.println("PLAYER NAME: " + player.getName());
+        System.out.println("JOB: " + player.getJob());
+        System.out.println("\n" + "Modify [M]" + "\n" + "Ok [O]");
 
         while (true) {
             try {
                 userValue = scanner.nextLine().toUpperCase();
 
-                if (userValue.equals("P")) {
-                    isModify = true;
-                    return 1;
-                }
-
-                if (userValue.equals("J")) {
-                    return 2;
-                }
-
-                if (userValue.equals("B")) {
+                if (userValue.equals("M")) {
                     return 4;
+                }
+
+                if (userValue.equals("O")) {
+                    return 6;
                 }
 
                 throw new Exception();
